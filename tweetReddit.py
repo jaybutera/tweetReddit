3275d32fe4a3fcdeaa8b8114ab2efd0a80af023d
Jay Butera <buterajay@gmail.com>
2014-05-20 01:34:54 -0500

    Organize error handling

--- tweetReddit.py
+++ tweetReddit.py
@@ -3,36 +3,43 @@ import requests
 import json
 import re
 
+def hashIt(s):
+    for word in s:
+        if len(word) > 8:
+            word = ''.join(['#', word])
+    return s
+
+def standard(post):
+    if len('\n'.join([post['title'], post['url']])) <= 140:
+        try:
+            txt = '\n'.join([post['title'], post['url']])
+            txt = hashIt(txt)
+            if txt not in pub:
+                api.update_status('\n'.join([post['title'], post['url']]))
+        except Exception, e:
+            return 0
+
+def short(post):
+    match = re.match(r'^.*?[\.!\?](?:\s|$)',post['title']).group(0)
+
+    if len('\n'.join([match, post['url']])) <= 140 and match is not None:
+        txt = '\n'.join([match, post['url']])
+        txt = hashIt(txt)
+        if txt not in pub:
+            api.update_status('\n'.join([match, post['url']]))
+            return 1
+        else: return 0
+    else:
+        return 0
+
 def tweet(i, data):
     pub = api.home_timeline()
     post = data['data']['children'][i]['data']
 
-    try:
-        if len('\n'.join([post['title'], post['url']])) <= 140:
-            try:
-                if '\n'.join([post['title'], post['url']]) not in pub:
-                    api.update_status('\n'.join([post['title'], post['url']]))
-            except Exception, e:
-                print "It's too long"
-                tweet(i+1, data)
-            pass
-        elif len('\n'.join([re.search(r'^.*?[\.!\?](?:\s|$)',post['title']).group(0),
-            post['url']])) <= 140:
-            try:
-                if '\n'.join([re.search(r'^.*?[\.!\?](?:\s|$)',post['title']).group(0), post['url']]) not in pub:
-                    api.update_status('\n'.join([re.search(r'^.*?[\.!\?](?:\s|$)',post['title']).group(0), post['url']]))
-            except Exception, e:
-                print 'Not even the first sentence.'
-                tweet(i+1, data)
-            pass
-        else:
-            print i
-            print 'Could not resolve 140 rule issue.'
-            tweet(i+1, data)
-    except Exception, e:
-        print 'Could not resolve 140 rule.'
-        tweet(i+1, data)
-        pass
+    if standard(post) == 1:
+        return 1
+    elif short(post) == 1:
+        return 1
 
 if __name__ == '__main__':
     consumer_key = 'mq37G4cLvQ3TDCzTKYwCATJkk'
